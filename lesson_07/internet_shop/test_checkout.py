42820e3e39d228b77d635e0217fdb10c58e9c474
Xenta71 <xenta.sds@gmail.com>
2025-05-24 18:39:15 +0300

    lesson_07 commit

--- lesson_07/internet_shop/test_checkout.py
+++ lesson_07/internet_shop/test_checkout.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import LoginPage
-
+from selenium.webdriver.common.by import By
+from pages.inventory_page import InventoryPage
 
 @pytest.fixture
 def driver():
